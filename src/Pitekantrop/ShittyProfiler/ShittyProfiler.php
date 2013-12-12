98d53d947898cba8baa0a2c74ff87476378b8de2
pitekantrop <pitekantrop@gmx.com>
2013-12-12 20:06:09 +0100

    init

---
+++ src/Pitekantrop/ShittyProfiler/ShittyProfiler.php
@@ -0,0 +1,170 @@
+<?php namespace Pitekantrop\ShittyProfiler;
+
+
+class ShittyProfiler {
+
+	protected $startTime;
+	protected $endTime;
+	protected $app;
+	static $markers = array();
+
+	/**
+	 * Set initial params
+	 * 
+	 * @param integer $endTime
+	 * @param Illuminate\Foundation\Application $app
+	 */
+	public function __construct($endTime, $app)
+	{
+		$this->startTime = LARAVEL_START;
+		$this->endTime = $endTime;
+		$this->app = $app;
+	}
+
+	//--------------------------------------------------------------------
+	
+	/**
+	 * Replace the response output with the profiler view
+	 * 
+	 * @return void
+	 */
+	public function displayProfiler($res)
+	{	
+		$data['timestamp'] = $this->getGlobalTime();
+		$data['memory']    = $this->getMemoryUsage();
+		$data['markers']   = $this->getMarkerPoints();
+		$data['files']     = $this->getIncludedFiles();
+		$data['db']        = $this->getQueryStrings();
+		
+		$res->header('Content-Type', 'text/html');
+		$res->setContent($this->app['view']->make('shittyprofiler::prof', $data));
+	}
+
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Set a marker point
+	 * 
+	 * @param  string $mark
+	 * @return void
+	 */
+	public static function mark($mark)
+	{
+		static::$markers[$mark] = microtime(true);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Prepare all marker points that are set by Profiler::mark()
+	 * 
+	 * @return array
+	 */
+	protected function getMarkerPoints()
+	{
+		$markers = array();
+
+		foreach (static::$markers as $mark => $time) 
+		{
+			$endMark = $mark.'End';
+
+			if (array_key_exists($endMark, static::$markers)) 
+			{
+				$time = static::$markers[$endMark] - $time;
+			}
+			else
+			{
+				$mark = 'start -> '.$mark;
+				$time = $time - $this->startTime;
+			}
+
+			if (!strpos($endMark, 'EndEnd')) 
+			{
+				$markers[] = array(
+					'name' => $mark,
+					'time' => $this->formatTime($time),
+				);
+			}
+		}
+
+		return $markers;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Calculate the elapsed time since LARAVEL_START
+	 * 
+	 * @return float
+	 */
+	protected function getGlobalTime()
+	{
+		return $this->formatTime($this->endTime - $this->startTime);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Convert microseconds to milliseconds
+	 * 
+	 * @param  integer  $timestamp 
+	 * @param  integer  $points    
+	 * @return float
+	 */
+	public function formatTime($timestamp, $points = 2)
+	{
+		return number_format($timestamp * 1000, $points);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Get query strings and format them to be more informative
+	 * 
+	 * @return array
+	 */
+	protected function getQueryStrings()
+	{
+		$queries = $this->app->make('db')->getQueryLog();
+		
+		$db['queries'] = array();
+		$db['total'] = 0;
+
+		foreach ($queries as $i => $query) 
+		{
+			$db['total'] += $query['time'];
+
+			$db['queries'][] = array(
+				'time'  => $query['time'],
+				'query' => preg_replace('/\?/e', 'array_shift($query["bindings"])', $query['query']),
+			);
+		}
+
+		return $db;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Get a list of included files
+	 * 
+	 * @return array
+	 */
+	protected function getIncludedFiles()
+	{
+		return get_included_files();
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Get memory usage info;
+	 * 
+	 * @return float
+	 */
+	protected function getMemoryUsage()
+	{
+		return number_format(memory_get_usage() / 1048576, 2);
+	}
+}
---
+++ src/Pitekantrop/ShittyProfiler/ShittyprofilerServiceProvider.php
@@ -0,0 +1,58 @@
+<?php namespace Pitekantrop\ShittyProfiler;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class ShittyProfilerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		AliasLoader::getInstance()->alias('Profiler', 'Pitekantrop\ShittyProfiler\ShittyProfiler');
+
+		if ($this->profilerEnabled()) 
+		{
+			$app =& $this->app;
+			$this->app->after(function($req, $res) use ($app)
+			{
+				$prof = new ShittyProfiler(microtime(true), $app);
+				$prof->displayProfiler($res);
+			});
+
+			
+			$this->package('pitekantrop/shittyprofiler');
+		}
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+	/**
+	 * Check if we should profile this request
+	 * 
+	 * @return boolean
+	 */
+	public function profilerEnabled()
+	{
+		return array_key_exists('profile', $_GET);
+	}
+
+}
